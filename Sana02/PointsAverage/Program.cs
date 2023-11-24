9a86a262c034c955e586a50bc9fe2d431a51a4f1
Dmytro Danylkovych <ipz233_dds@student.ztu.edu.ua>
2023-11-24 14:40:01 +0200

    Points Average

---
+++ Sana02/PointsAverage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+
+int sum = 0;
+
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine($"Введіть оцінку {i}: ");
+    int a = int.Parse(Console.ReadLine());
+    if (a < 0 && a > 100)
+    {
+        Console.WriteLine("Ви некоректно ввели оцінку");
+        i--;
+    }
+    else
+        sum += a;
+}
+
+int avarage = sum / 5;
+
+Console.WriteLine($"Ваша середня оцінка: {avarage}");
+
+if (avarage >= 50)
+    Console.WriteLine("Ви допущені до екзамену");
+else
+    Console.WriteLine("Ви не допущені до екзамену");
