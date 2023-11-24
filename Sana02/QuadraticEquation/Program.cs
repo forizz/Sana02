e3a1ef704592db21ab3913d57839d917c231b048
Dmytro Danylkovych <ipz233_dds@student.ztu.edu.ua>
2023-11-24 14:37:15 +0200

    Quadratic Equation

---
+++ Sana02/QuadraticEquation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+Console.WriteLine("Введіть число a: ");
+var a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число b: ");
+var b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть число c: ");
+var c = int.Parse(Console.ReadLine());
+
+int D = b*b - 4*a*c;
+double x1, x2;
+if (D > 0)
+{
+    x1 = (-b + Math.Sqrt(D)) / (2*a);
+    x2 = (-b - Math.Sqrt(D)) / (2*a);
+    Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+}
+else if (D == 0)
+{
+    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+    Console.WriteLine($"x = {x1}");
+}
+else
+{
+    Console.WriteLine("Немає коренів");
+}
